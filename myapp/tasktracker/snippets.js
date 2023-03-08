2a871cc891921d5dbd38b451a4e86ba8db9ef4bb
Алина Фахердинова <fakherdinova.alya@mail.ru>
2023-03-08 15:10:29 +0300

    new

--- myapp/tasktracker/snippets.js
+++ myapp/tasktracker/snippets.js
@@ -1,66 +1,25 @@
-/*const url = 'http://127.0.0.1:8000/api/create/'
+/*Get
 
+fetch('http://127.0.0.1:8000/api/get_all/',
+	{headers: {'Authorization': `Bearer ${token}`}})
+
+     .then(res => res.json())
+     .then(data => console.log(data))*/
+
+/*Put
+const url = 'http://127.0.0.1:8000/api/delete/2'
 let obj = {
-    id: 3,
-    name: 'd',
+    id: 2,
+    name: 'lunch',
     description: 'a',
     status: 'Done',
     date: "2023-03-02"
  }
  fetch(url, {
-     method: 'POST',
-     headers: {
-         'Content-Type': 'application/json;charset=utf-8'
-     },
-     body: JSON.stringify(obj)
- }*/
-let obj = {
-    id: 3,
-    name: 'd',
-    description: 'a',
-    status: 'Done',
-    date: "2023-03-02"
- }
- fetch(url, {
-     method: 'POST',
+     method: 'PUT',
      headers: {
          'Authorization': `Bearer ${token}`,
          'Content-Type': 'application/json;charset=utf-8'
      },
      body: JSON.stringify(obj)
- })
-
-
-
-const url = 'http://127.0.0.1:8000/api/delete/3'
-
-
-let obj = {
-    id: 1,
-    name: 'd',
-    description: 'a',
-    status: 'Done',
-    date: "2023-03-02"
- }
-fetch(url, {
-    method: 'UPDATE',
-    headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json;charset=utf-8'
-    }
-})
-     .then(res => res.json())
-     .then(data => console.log(data))
-
-const url = 'http://127.0.0.1:8000/api/delete/2'
-fetch(url, {
-    method: 'PUT',
-
-    headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json;charset=utf-8'
-    }
-
-} )
-     .then(res => res.json())
-     .then(data => console.log(data))
+ })*/
